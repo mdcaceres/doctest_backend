bdb9d6cbee93ab9dab70ce4ad33842e897d5dd53
mdcaceres <you@example.com>
2023-04-14 23:01:21 -0300

    fix url mapping

--- models/project.go
+++ models/project.go
@@ -6,10 +6,10 @@ import (
 
 type Project struct {
 	gorm.Model
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	UserId      uint     `json:"owner_id"`
-	Team        []*User  `json:"users,omitempty" gorm:"many2many:project_team"`
-	Suites      []*Suite `json:"suites,omitempty" gorm:"many2many:project_suites"`
-	Image       []byte   `json:"image"`
+	Name        string
+	Description string
+	UserId      uint
+	Team        []*User  `gorm:"many2many:project_team"`
+	Suites      []*Suite `gorm:"many2many:project_suites"`
+	Image       []byte
 }
